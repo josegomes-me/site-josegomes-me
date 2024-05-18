91778c52bccb8e3eb5dbcae33c3d5dde4fe01970
Jose Gomes <josegomes@wemakeit.es>
2024-05-18 23:41:51 +0100

    Webpack Structure Project and teste project

--- src/main.js
+++ src/main.js
@@ -5,4 +5,13 @@ import axios from 'axios';
 window.Alpine = Alpine;
 window.axios = axios;
 
+document.addEventListener('alpine:init', () => {
+    Alpine.data('modal', () => ({
+        open: false,
+        toggle() {
+            this.open = !this.open;
+        }
+    }));
+});
+
 Alpine.start();
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,44 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: './src/main.js',
     output: {
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'postcss-loader',
-                ]
-            }
-        ]
-    }
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9001,
+    },
 };
