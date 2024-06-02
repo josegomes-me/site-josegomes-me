fff35b0e7a18b040679445ba515cfb360f311fa0
Jose Gomes <josegomes@wemakeit.es>
2024-06-02 17:43:22 +0100

    Change version Alpinejs

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,8 @@ module.exports = {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env'],
-                    },
+                        plugins: ['@babel/plugin-transform-runtime']
+                    }
                 },
             },
         ],
